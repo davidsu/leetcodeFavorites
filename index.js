9e2020eb12b3170ce03f46791686600cb8dfa409
David <davidsu@wix.com>
2019-04-21 07:25:30 +0300

    get random from most liked problems instead

--- index.js
+++ index.js
@@ -2,24 +2,35 @@
 const Plugin = require('./node_modules/leetcode-cli/lib/plugin')
 const cli = require('./node_modules/leetcode-cli/lib/cli')
 const core = require('./node_modules/leetcode-cli/lib/core')
+const cp = require('child_process')
 
-function getNewProblem(error, problems, problem) {
+function sortProblemByLike(error, problems, problem) {
     if(!problems) {
-        return core.filterProblems({query: 'D'}, getNewProblem) 
+        return new Promise(r => {
+            core.filterProblems({query: 'D'}, (a,b,c) => {
+                r(sortProblemByLike(a,b,c))
+            })
+        })
     }
-    if(!problem || problem.likes < problem.dislikes) {
-        problem = problems[Math.floor(Math.random() * problems.length -1)]
-        return core.getProblem(problem.id, (e, problem) => getNewProblem(e, problems, problem))
+    const problemsLikes = []
+    let prom = Promise.resolve()
+    for(let p of problems) {
+        // core.getProblem(p.id, () => {})
+        prom = prom.then(() => new Promise(r => {
+            core.getProblem(p.id, (e, problem) => {
+                if(!e) {
+                    let {likes, dislikes, link} = problem
+                    problemsLikes.push({likes, dislikes, link})
+                }
+                r()
+            })
+        })).catch()
     }
-    if(error)
-        throw error
-    else {
-        if(problem.dislikes > problem.likes) {
-            return getNewProblem()
-        }
-        require('child_process').execSync(`open ${problem.link}`)
-
-    }
+    debugger
+    return prom.then( () => {
+        const res = problemsLikes.sort((a, b) => b.likes - a.likes)
+        return problemsLikes
+    })
 }
 function getProblemFromReviewList() {
     Plugin.plugins[2].getFavorites((_, {favorites}) => {
@@ -33,11 +44,20 @@ function getProblemFromReviewList() {
 
 cli.run()
 
-// getNewProblem()
-// getProblemFromReviewList()
-if(Math.floor(Math.random() * 6) == 1) {
-    getNewProblem()
+const getRandom = () => {
+    return sortProblemByLike().then(p => {
+        const idx = Math.floor(Math.random() * 20)
+        cp.execSync(`open ${p[idx].link}`)
+        return p
+    })
+}
+if(process.argv.length > 2) {
+    getRandom().then(p => p.forEach(p =>console.log(JSON.stringify(p))))
+} else if(Math.floor(Math.random() * 5) == 1) {
+    console.log('getting random')
+    getRandom()
 } else {
+    console.log('getting from list')
     getProblemFromReviewList()
 }
 
