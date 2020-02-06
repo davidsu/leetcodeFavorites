e057d2ad9da024d332c9276bdf23af8f222ddd34
David <david.susskind@walkme.com>
2020-02-06 17:44:34 +0200

    117.populating-next-right-pointers using `next` as bfsQueue

---
+++ src/117.populating-next-right-pointers-in-each-node-ii.js
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode id=117 lang=javascript
+ *
+ * [117] Populating Next Right Pointers in Each Node II
+ *
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/description/
+ *
+ * algorithms
+ * Medium (35.60%)
+ * Total Accepted:    201.4K
+ * Total Submissions: 565.6K
+ * Testcase Example:  '{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}'
+ *
+ * Given a binary tree
+ *
+ *
+ * struct Node {
+ * ⁠ int val;
+ * ⁠ Node *left;
+ * ⁠ Node *right;
+ * ⁠ Node *next;
+ * }
+ *
+ *
+ * Populate each next pointer to point to its next right node. If there is no
+ * next right node, the next pointer should be set to NULL.
+ *
+ * Initially, all next pointers are set to NULL.
+ *
+ *
+ *
+ * Example:
+ *
+ *
+ *
+ *
+ * Input:
+ * {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+ *
+ * Output:
+ * {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":null,"right":null,"val":7},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"6","left":null,"next":null,"right":{"$ref":"5"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"6"},"val":1}
+ *
+ * Explanation: Given the above binary tree (Figure A), your function should
+ * populate each next pointer to point to its next right node, just like in
+ * Figure B.
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * You may only use constant extra space.
+ * Recursive approach is fine, implicit stack space does not count as extra
+ * space for this problem.
+ *
+ */
+/**
+ * // Defnition for a Node.
+ * function Node(val,left,right,next) {
+ *    this.val = val;
+ *    this.left = left;
+ *    this.right = right;
+ *    this.next = next;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+function connect (root) {
+    let nextRoot = {}
+    let needle = nextRoot
+    const head = root
+    while (root) {
+        if (root.left) {
+            needle.next = root.left
+            needle = needle.next
+        }
+        if (root.right) {
+            needle.next = root.right
+            needle = needle.next
+        }
+        root = root.next
+        if (!root) {
+            root = nextRoot.next
+            nextRoot = {}
+            needle = nextRoot
+        }
+    }
+    return head
+}
+
+module.exports = {
+    connect
+}
---
+++ test/117.populating-next-right-pointers-in-each-node-ii.test.js
@@ -0,0 +1,11 @@
+const testFuncs = require('../src/117.populating-next-right-pointers-in-each-node-ii.js')
+describe.each(
+    Object.entries(testFuncs)
+)('%s', (_, testFunc) => {
+    it('case 1', () => {
+        const input = { val: 1, left: { val: 2, left: { val: 4, left: null, right: null, next: null }, right: { val: 5, left: null, right: null, next: null }, next: null }, right: { val: 3, left: null, right: { val: 7, left: null, right: null, next: null }, next: null }, next: null }
+        const expected = { val: 1, left: { val: 2, left: { val: 4, left: null, right: null, next: { val: 5, left: null, right: null, next: { val: 7, left: null, right: null, next: null } } }, right: { val: 5, left: null, right: null, next: { val: 7, left: null, right: null, next: null } }, next: { val: 3, left: null, right: { val: 7, left: null, right: null, next: null }, next: null } }, right: { val: 3, left: null, right: { val: 7, left: null, right: null, next: null }, next: null }, next: null }
+        const result = testFunc(input)
+        expect(result).toEqual(expected)
+    })
+})
