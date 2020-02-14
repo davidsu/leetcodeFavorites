450b0094b4213a42f7921d6003656e585d0c027d
David <david.susskind@walkme.com>
2020-02-14 15:53:56 +0200

    234.palindrome-linked-list O(n) time O(1) space - mutates input

---
+++ src/234.palindrome-linked-list.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=234 lang=javascript
+ *
+ * [234] Palindrome Linked List
+ *
+ * https://leetcode.com/problems/palindrome-linked-list/description/
+ *
+ * algorithms
+ * Easy (37.07%)
+ * Total Accepted:    349K
+ * Total Submissions: 921K
+ * Testcase Example:  '[1,2]'
+ *
+ * Given a singly linked list, determine if it is a palindrome.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 1->2
+ * Output: false
+ *
+ * Example 2:
+ *
+ *
+ * Input: 1->2->2->1
+ * Output: true
+ *
+ * Follow up:
+ * Could you do it in O(n) time and O(1) space?
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const reverse = (head, prev = null) => {
+    const next = head.next
+    head.next = prev
+    return next ? reverse(next, head) : head
+}
+var isPalindrome = function (head) {
+    if (!head || !head.next) return true
+    let slow = head; let fast = head.next
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+    }
+    slow.next = reverse(slow.next)
+    slow = slow.next
+    while (slow && head.val === slow.val) {
+        slow = slow.next
+        head = head.next
+    }
+    return !slow
+}
+
+module.exports = {
+    isPalindrome
+}
---
+++ test/234.palindrome-linked-list.test.js
@@ -0,0 +1,16 @@
+const testFuncs = require('../src/234.palindrome-linked-list.js')
+describe.each(
+    Object.entries(testFuncs)
+)('%s', (_, testFunc) => {
+    it('case 1', () => {
+        expect(testFunc({ val: 1, next: { val: 2, next: null } })).toEqual(false)
+    })
+    it('case 2', () => {
+        const list = { val: 1, next: { val: 2, next: { val: 2, next: { val: 1, next: null } } } }
+        expect(testFunc(list)).toEqual(true)
+    })
+    it('case 3', () => {
+        const list = { val: 1, next: { val: 2, next: { val: 1, next: { val: 2, next: { val: 1, next: null } } } } }
+        expect(testFunc(list)).toEqual(true)
+    })
+})
