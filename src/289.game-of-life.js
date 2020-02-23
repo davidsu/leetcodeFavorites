f5e922eb711bb1583aeee6a36603ba601c8f3cc6
David <david.susskind@walkme.com>
2020-02-23 22:22:38 +0200

    289.game-of-life.test unelegant inplace solution

---
+++ src/289.game-of-life.js
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode id=289 lang=javascript
+ *
+ * [289] Game of Life
+ *
+ * https://leetcode.com/problems/game-of-life/description/
+ *
+ * algorithms
+ * Medium (47.99%)
+ * Total Accepted:    151.9K
+ * Total Submissions: 299.5K
+ * Testcase Example:  '[[0,1,0],[0,0,1],[1,1,1],[0,0,0]]'
+ *
+ * According to the Wikipedia's article: "The Game of Life, also known simply
+ * as Life, is a cellular automaton devised by the British mathematician John
+ * Horton Conway in 1970."
+ *
+ * Given a board with m by n cells, each cell has an initial state live (1) or
+ * dead (0). Each cell interacts with its eight neighbors (horizontal,
+ * vertical, diagonal) using the following four rules (taken from the above
+ * Wikipedia article):
+ *
+ *
+ * Any live cell with fewer than two live neighbors dies, as if caused by
+ * under-population.
+ * Any live cell with two or three live neighbors lives on to the next
+ * generation.
+ * Any live cell with more than three live neighbors dies, as if by
+ * over-population..
+ * Any dead cell with exactly three live neighbors becomes a live cell, as if
+ * by reproduction.
+ *
+ *
+ * Write a function to compute the next state (after one update) of the board
+ * given its current state. The next state is created by applying the above
+ * rules simultaneously to every cell in the current state, where births and
+ * deaths occur simultaneously.
+ *
+ * Example:
+ *
+ *
+ * Input:
+ * [
+ * [0,1,0],
+ * [0,0,1],
+ * [1,1,1],
+ * [0,0,0]
+ * ]
+ * Output:
+ * [
+ * [0,0,0],
+ * [1,0,1],
+ * [0,1,1],
+ * [0,1,0]
+ * ]
+ *
+ *
+ * Follow up:
+ *
+ *
+ * Could you solve it in-place? Remember that the board needs to be updated at
+ * the same time: You cannot update some cells first and then use their updated
+ * values to update other cells.
+ * In this question, we represent the board using a 2D array. In principle, the
+ * board is infinite, which would cause problems when the active area
+ * encroaches the border of the array. How would you address these problems?
+ *
+ *
+ */
+/**
+ * @param {number[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+const gameOfLife = function (board) {
+    const isDead = (i, j) => board[i][j] === 0 || board[i][j] >= 20
+    const isAlive = (i, j) => !isDead(i, j)
+    const getNeighbors = (i, j) => [
+        [i - 1, j - 1],
+        [i - 1, j],
+        [i - 1, j + 1],
+        [i, j - 1],
+        [i, j + 1],
+        [i + 1, j - 1],
+        [i + 1, j],
+        [i + 1, j + 1]
+    ].filter(([i, j]) => i in board && j in board[i])
+    const getLiveNeighborCount = (i, j) => getNeighbors(i, j).map(([i, j]) => isAlive(i, j)).reduce((a, b) => a + b, 0)
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            const alive = !!board[i][j]
+            const aliveIndicator = alive ? 10 : 20
+            const liveNeighbotCount = getLiveNeighborCount(i, j)
+            const willLive = (alive && (liveNeighbotCount === 2 || liveNeighbotCount === 3)) || (!alive && liveNeighbotCount === 3)
+            board[i][j] = aliveIndicator + willLive
+        }
+    }
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            board[i][j] = board[i][j] % 2
+        }
+    }
+}
+
+module.exports = {
+    gameOfLife
+}
---
+++ test/289.game-of-life.test.js
@@ -0,0 +1,23 @@
+const testFuncs = require('../src/289.game-of-life.js')
+describe.each(
+    Object.entries(testFuncs)
+)('%s', (_, testFunc) => {
+    it('case 1', () => {
+        const board =
+            [
+                [0, 1, 0],
+                [0, 0, 1],
+                [1, 1, 1],
+                [0, 0, 0]
+            ]
+        const expected =
+            [
+                [0, 0, 0],
+                [1, 0, 1],
+                [0, 1, 1],
+                [0, 1, 0]
+            ]
+        testFunc(board)
+        expect(board).toEqual(expected)
+    })
+})
