dd49c512c79b57e46e1c55b8fedac580acb08787
David <david.susskind@walkme.com>
2020-04-02 17:19:02 +0300

    happy number

---
+++ test/202.happy-number.test.js
@@ -0,0 +1,8 @@
+const testFuncs = require('../src/202.happy-number')
+describe.each(
+    Object.entries(testFuncs)
+)('%s', (_, testFunc) => {
+    it('case 1', () => {
+        expect(testFunc(19)).toEqual(true)
+    })
+})
