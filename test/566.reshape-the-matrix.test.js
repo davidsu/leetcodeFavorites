261c99b0e445323bd4479264769ced49d7afe3d6
David <david.susskind@walkme.com>
2020-05-01 20:36:17 +0300

    solve reshape the matrix

---
+++ test/566.reshape-the-matrix.test.js
@@ -0,0 +1,11 @@
+const testFuncs = require('../src/566.reshape-the-matrix.js')
+describe.each(
+    Object.entries(testFuncs)
+)('%s', (_, testFunc) => {
+    it('case 1', () => {
+        expect(testFunc([[1, 2], [3, 4]], 1, 4)).toEqual([[1, 2, 3, 4]])
+    })
+    it('case 2', () => {
+        expect(testFunc([[1, 2], [3, 4]], 2, 4)).toEqual([[1, 2], [3, 4]])
+    })
+})
