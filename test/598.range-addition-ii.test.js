767ad325a53e4c5ce85ecf88fb06f605c28d5404
David <david.susskind@walkme.com>
2020-05-01 21:10:24 +0300

    solve range-addition

---
+++ test/598.range-addition-ii.test.js
@@ -0,0 +1,8 @@
+const testFuncs = require('../src/598.range-addition-ii.js')
+describe.each(
+    Object.entries(testFuncs)
+)('%s', (_, testFunc) => {
+    it('case 1', () => {
+        expect(testFunc(3, 3, [[2, 2], [3, 3]])).toEqual(4)
+    })
+})
