8f016fa8b357ac67737b56e2e38573f910dea3b5
David <david.susskind@walkme.com>
2020-05-03 18:49:25 +0300

    solve "jewels and stones"

---
+++ src/771.jewels-and-stones.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=771 lang=javascript
+ *
+ * [771] Jewels and Stones
+ *
+ * https://leetcode.com/problems/jewels-and-stones/description/
+ *
+ * algorithms
+ * Easy (85.41%)
+ * Total Accepted:    439.6K
+ * Total Submissions: 514.7K
+ * Testcase Example:  '"aA"\n"aAAbbbb"'
+ *
+ * You're given strings J representing the types of stones that are jewels, and
+ * S representing the stones you have.  Each character in S is a type of stone
+ * you have.  You want to know how many of the stones you have are also
+ * jewels.
+ *
+ * The letters in J are guaranteed distinct, and all characters in J and S are
+ * letters. Letters are case sensitive, so "a" is considered a different type
+ * of stone from "A".
+ *
+ * Example 1:
+ *
+ *
+ * Input: J = "aA", S = "aAAbbbb"
+ * Output: 3
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: J = "z", S = "ZZ"
+ * Output: 0
+ *
+ *
+ * Note:
+ *
+ *
+ * S and J will consist of letters and have length at most 50.
+ * The characters in J are distinct.
+ *
+ *
+ */
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+const numJewelsInStones = (J, S) => {
+    const jewels = new Set(J.split(''))
+    return S.split('').filter(stone => jewels.has(stone)).length
+}
+module.exports = { numJewelsInStones }
