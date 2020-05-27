d671f5af9424bb89c2fde7fa59651e4fbec2bc77
David <david.susskind@walkme.com>
2020-05-27 03:04:51 +0300

    solve rotate string

---
+++ test/796.rotate-string.test.js
@@ -0,0 +1,17 @@
+const testFuncs = require('../src/796.rotate-string.js')
+describe.each(
+    Object.entries(testFuncs)
+)('%s', (_, testFunc) => {
+    it('case 1', () => {
+        expect(testFunc('abcde', 'cdeab')).toEqual(true)
+    })
+    it('case 2', () => {
+        expect(testFunc('abcde', 'abced')).toEqual(false)
+    })
+    it('case 3', () => {
+        expect(testFunc('', '')).toEqual(true)
+    })
+    it('case 4', () => {
+        expect(testFunc('a', 'aa')).toEqual(false)
+    })
+})
