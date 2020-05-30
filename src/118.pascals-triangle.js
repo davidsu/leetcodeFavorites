bb4979cd86fe2930416b7f10197e29cc73a53141
David <david.susskind@walkme.com>
2020-05-30 23:43:14 +0300

    pascal triangle

---
+++ src/118.pascals-triangle.js
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=118 lang=javascript
+ *
+ * [118] Pascal's Triangle
+ *
+ * https://leetcode.com/problems/pascals-triangle/description/
+ *
+ * algorithms
+ * Easy (51.30%)
+ * Total Accepted:    362.6K
+ * Total Submissions: 706.6K
+ * Testcase Example:  '5'
+ *
+ * Given a non-negative integer numRows, generate the first numRows of Pascal's
+ * triangle.
+ *
+ *
+ * In Pascal's triangle, each number is the sum of the two numbers directly
+ * above it.
+ *
+ * Example:
+ *
+ *
+ * Input: 5
+ * Output:
+ * [
+ * ⁠    [1],
+ * ⁠   [1,1],
+ * ⁠  [1,2,1],
+ * ⁠ [1,3,3,1],
+ * ⁠[1,4,6,4,1]
+ * ]
+ *
+ *
+ */
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+const get = (arr = [], i) => arr[i] || 0
+const generate = function (numRows) {
+    const result = new Array(numRows)
+    for (let i = 0; i < numRows; i++) {
+        result[i] = new Array(i + 1).fill(0).map((val, idx) => (get(result[i - 1], idx - 1) + get(result[i - 1], idx)) || 1)
+    }
+    return result
+}
+
+module.exports = {
+    generate
+}
---
+++ test/118.pascals-triangle.test.js
@@ -0,0 +1,16 @@
+const testFuncs = require('../src/118.pascals-triangle.js')
+describe.each(
+    Object.entries(testFuncs)
+)('%s', (_, testFunc) => {
+    it('case 1', () => {
+        expect(testFunc(5)).toEqual(
+            [
+                [1],
+                [1, 1],
+                [1, 2, 1],
+                [1, 3, 3, 1],
+                [1, 4, 6, 4, 1]
+            ]
+        )
+    })
+})
