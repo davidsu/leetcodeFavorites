c4eaea80bcf25530850cca98c8c5b9328b6ca330
David <david.susskind@walkme.com>
2020-05-31 00:51:48 +0300

    fix lint

--- test/961.n-repeated-element-in-size-2n-array.test.js
+++ test/961.n-repeated-element-in-size-2n-array.test.js
@@ -3,12 +3,12 @@ describe.each(
     Object.entries(testFuncs)
 )('%s', (_, testFunc) => {
     it('case 1', () => {
-        expect(testFunc([1,2,3,3])).toEqual(3)
+        expect(testFunc([1, 2, 3, 3])).toEqual(3)
     })
     it('case 2', () => {
-        expect(testFunc([2,1,2,5,3,2])).toEqual(2)
+        expect(testFunc([2, 1, 2, 5, 3, 2])).toEqual(2)
     })
     it('case 1', () => {
-        expect(testFunc([5,1,5,2,5,3,5,4])).toEqual(5)
+        expect(testFunc([5, 1, 5, 2, 5, 3, 5, 4])).toEqual(5)
     })
 })
