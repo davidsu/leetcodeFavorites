e8a7b2ab1828531fd9d2eae917dc82058689f807
David <david.susskind@walkme.com>
2020-05-31 00:30:03 +0300

    k closest points

---
+++ test/973.k-closest-points-to-origin.test.js
@@ -0,0 +1,13 @@
+const testFuncs = require('../src/973.k-closest-points-to-origin')
+describe.each(
+    Object.entries(testFuncs)
+)('%s', (_, testFunc) => {
+    it('case 1', () => {
+        expect(testFunc([[1, 3], [-2, 2]], 1)).toEqual([[-2, 2]])
+    })
+    it('case 2', () => {
+        const result = new Set(testFunc([[3, 3], [5, -1], [-2, 4]], 2))
+        const expected = new Set([[3, 3], [-2, 4]])
+        expect(result).toEqual(expected)
+    })
+})
