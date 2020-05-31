99d661900c1cb96047cf0f3c016b03672c52f32a
David <david.susskind@walkme.com>
2020-06-01 01:39:36 +0300

    fizzbuzz

---
+++ src/412.fizz-buzz.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=412 lang=javascript
+ *
+ * [412] Fizz Buzz
+ *
+ * https://leetcode.com/problems/fizz-buzz/description/
+ *
+ * algorithms
+ * Easy (61.77%)
+ * Total Accepted:    318K
+ * Total Submissions: 514.7K
+ * Testcase Example:  '1'
+ *
+ * Write a program that outputs the string representation of numbers from 1 to
+ * n.
+ *
+ * But for multiples of three it should output “Fizz” instead of the number and
+ * for the multiples of five output “Buzz”. For numbers which are multiples of
+ * both three and five output “FizzBuzz”.
+ *
+ * Example:
+ *
+ * n = 15,
+ *
+ * Return:
+ * [
+ * ⁠   "1",
+ * ⁠   "2",
+ * ⁠   "Fizz",
+ * ⁠   "4",
+ * ⁠   "Buzz",
+ * ⁠   "Fizz",
+ * ⁠   "7",
+ * ⁠   "8",
+ * ⁠   "Fizz",
+ * ⁠   "Buzz",
+ * ⁠   "11",
+ * ⁠   "Fizz",
+ * ⁠   "13",
+ * ⁠   "14",
+ * ⁠   "FizzBuzz"
+ * ]
+ *
+ *
+ */
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const fizzBuzz = n => Array.from({ length: n }, (_, i) => (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || `${i}`)
+
+module.exports = {
+    fizzBuzz
+}
