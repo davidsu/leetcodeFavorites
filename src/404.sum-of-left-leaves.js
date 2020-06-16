1c4416f559abd40cedc47479587a86fc7b1925dd
David <david.susskind@walkme.com>
2020-06-17 00:24:01 +0300

    sum of left leaves

---
+++ src/404.sum-of-left-leaves.js
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=404 lang=javascript
+ *
+ * [404] Sum of Left Leaves
+ *
+ * https://leetcode.com/problems/sum-of-left-leaves/description/
+ *
+ * algorithms
+ * Easy (50.66%)
+ * Total Accepted:    170.5K
+ * Total Submissions: 336.5K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Find the sum of all left leaves in a given binary tree.
+ *
+ * Example:
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ * There are two left leaves in the binary tree, with values 9 and 15
+ * respectively. Return 24.
+ *
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const sumOfLeftLeaves = (root, sum = false) => {
+  if (!root) return 0
+  if (!root.left && !root.right) return sum && root.val
+  return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right, false)
+}
+
+module.exports = {
+  sumOfLeftLeaves
+}
