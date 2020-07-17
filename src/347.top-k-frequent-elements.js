06236b9b08a32391b451ee3325af9935ec7715b7
David <david.susskind@walkme.com>
2020-07-17 17:10:52 +0300

    Top K Frequent Elements

--- src/347.top-k-frequent-elements.js
+++ src/347.top-k-frequent-elements.js
@@ -6,9 +6,9 @@
  * https://leetcode.com/problems/top-k-frequent-elements/description/
  *
  * algorithms
- * Medium (56.69%)
- * Total Accepted:    247.7K
- * Total Submissions: 437K
+ * Medium (60.40%)
+ * Total Accepted:    404.5K
+ * Total Submissions: 666.5K
  * Testcase Example:  '[1,1,1,2,2,3]\n2'
  *
  * Given a non-empty array of integers, return the k most frequent elements.
@@ -34,6 +34,9 @@
  * You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
  * Your algorithm's time complexity must be better than O(n log n), where n is
  * the array's size.
+ * It's guaranteed that the answer is unique, in other words the set of the top
+ * k frequent elements is unique.
+ * You can return the answer in any order.
  *
  *
  */
@@ -43,14 +46,24 @@
  * @return {number[]}
  */
 const topKFrequent = function (nums, k) {
-  const frequencies = nums.reduce((acc, val) => {
-    acc[val] = (acc[val] || 0) + 1
-    return acc
-  }, {})
-  return Object.entries(frequencies)
-    .sort(([_, freq1], [_2, freq2]) => freq2 - freq1)
-    .map(([val]) => parseInt(val))
-    .splice(0, k)
+  const freq = [new Set()]
+  const map = {}
+  for (const num of nums) {
+    map[num] = (map[num] || 0) + 1
+    if (freq.length === map[num]) {
+      freq.push(new Set())
+    }
+    freq[map[num]].add(num)
+    freq[map[num] - 1].delete(num)
+  }
+  let result = []
+  while (result.length < k) {
+    result = result.concat([...freq.pop()])
+  }
+  while (result.length > k) {
+    result.pop()
+  }
+  return result
 }
 
 module.exports = {
